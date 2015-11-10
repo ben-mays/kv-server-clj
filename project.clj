bbc9d8a0a583c0179d95f06ee7e7bbe2630ea480
ben-mays <ben-mays@eastside.io>
2015-11-10 15:47:11 -0500

    Adding ring/ring-json support

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
+
 (defproject kv-store "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler kv-store.handler/app}
   :profiles
