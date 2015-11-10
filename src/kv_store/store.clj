7778cb5fc5df6986b6235967ee74f286c9e5a9d3
ben-mays <ben-mays@eastside.io>
2015-11-10 15:19:06 -0500

    Initial store protocol and simple implementation using a locking atom

---
+++ src/kv_store/store.clj
@@ -0,0 +1,53 @@
+(ns kv-store.store)
+
+(defprotocol IStore
+  "A abstract interface for interacting with the underlying KV store"
+  (setEntry [this key val] [this key val predicates] "Put val into the store, under the key given. Returns a map with :value set to the new value of the key, or :errors seto a error message, with :results as a list of the predicate evaluations.")
+  (getEntry [this key] [this key predicates] "Retrieves the value associated with key, returning a map with :value set to the value or :errors set to a error message."))
+
+;; We want to define a few predicates:
+;;  * equals key val
+;;  * exists key
+;;  * not-exists key
+
+;; Each predicate constructor returns an anonymous function that contains the args passed
+(defn pred-equals? [store key val]
+  #(= (:value (.getEntry store key)) val))
+
+(defn pred-exists? [store key]
+  #(not (nil? (:value (.getEntry store key)))))
+
+(defn pred-absent? [store key]
+  #(nil? (:value (.getEntry store key))))
+
+;; A simple KV store implementation based on a Clojure atom containing a map.
+;; Assumption that kv passed here is an atom; e.g. (def kv (atom {}))
+(deftype SimpleStore [^{:volatile-mutable true} kv] 
+  IStore
+
+  (setEntry 
+    [this key val]
+    (swap! kv assoc key val)
+    {:value val :errors nil})
+    
+  (setEntry 
+    [this key val predicates]
+    (locking kv 
+      (let [results (map #(%) predicates)]
+        (if (every? true? results)
+          ;; all of the predicates are true, perform the write
+          (.setEntry this key val)
+          {:value nil :errors "Predicate failed." :results results}))))
+
+  (getEntry 
+    [this key]
+    {:value (get @kv key) :errors nil})
+
+  (getEntry 
+    [this key predicates]
+    (locking kv
+      (let [results (map #(%) predicates)]
+        (if (every? true? results)
+          ;; all of the predicates are true, perform the read
+          (.getEntry this key)
+          {:value nil :errors "Predicates failed." :results results})))))
---
+++ test/kv_store/store_test.clj
@@ -0,0 +1,60 @@
+(ns kv-store.store-test
+  (:use [kv-store.store]
+        [clojure.test]))
+
+(def store-atom (atom {}))
+(def store (->SimpleStore store-atom))
+
+(defn init-test [] 
+  (reset! store-atom {}))
+
+(deftest predicate-tests
+  (testing "SetEntry Predicates"
+    (testing "Complex predicate happy case"
+      (init-test)
+      (def predicates [(pred-equals? store "1" "A") (pred-exists? store "2")]) ;; only update if "1" has value "A" and key "2" exists
+      (.setEntry store "1" "A") ;; set 1 => A
+      (.setEntry store "2" "B") ;; set 2 => B
+      (is (nil? (:errors (.setEntry store "3" "C" predicates)))) ;; set 3 => C if the predicates hold up
+      (is (= (:value (.getEntry store "1")) "A"))
+      (is (= (:value (.getEntry store "2")) "B"))
+      (is (= (:value (.getEntry store "3")) "C")))
+
+    (testing "equals happy case"
+      (init-test)
+      (.setEntry store "1" "A")
+      (is (nil? (:errors (.setEntry store "2" "B" [(pred-equals? store "1" "A")]))))
+      (is (= (:value (.getEntry store "1")) "A"))
+      (is (= (:value (.getEntry store "2")) "B")))
+
+    (testing "equals failure case" 
+      (init-test)
+      (is (not (nil? (:errors (.setEntry store "2" "B" [(pred-equals? store "1" "A")])))))
+      (is (nil? (:value (.getEntry store "2")))))
+    
+    (testing "exists happy case"
+      (init-test)
+      (.setEntry store "1" "A")
+      (is (nil? (:errors (.setEntry store "2" "B" [(pred-exists? store "1")]))))
+      (is (= (:value (.getEntry store "2")) "B")))
+    
+    (testing "exists failure case"
+      (init-test)
+      (is (not (nil? (:errors (.setEntry store "2" "B" [(pred-exists? store "2")])))))
+      (is (nil? (:value (.getEntry store "2")))))))
+
+(deftest predicate-read 
+  (testing "Version Didn't Change"
+    (init-test)
+    ;; lets assume this was placed into the store atomically, even though we can only write a single value at a time currently.
+    (.setEntry store "my-key" "a")
+    (.setEntry store "my-key-version" 1)
+    (is (= (:value (.getEntry store "my-key" [(pred-equals? store "my-key-version" 1)])) "a"))
+    ;; cool, we got the value back and we know that the version didn't change on us := thus we're consistent up to this point
+    
+    ;; Assuming two clients, b and c contending over my-key.
+    (.setEntry store "my-key" "b")
+    (.setEntry store "my-key-version" 2)
+    
+    ;; Now client C watns update the key with key + 1 iff the key-version hasn't changed. This should fail because client b has updated my-key.
+    (is (nil? (:value (.getEntry store "my-key-version" [(pred-equals? store "my-key-version" 1)]))))))
